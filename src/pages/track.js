64d75ffd488099a96dbbe95890fd75890b8c28ba
Luke <luke@luke.co.pl>
2021-12-14 11:13:06 +0100

    add subscription for track view refresh

--- src/pages/track.js
+++ src/pages/track.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { Layout, QueryResult } from '../components';
 import TrackDetail from '../components/track-detail';
@@ -28,14 +28,49 @@ export const GET_TRACK = gql`
   }
 `;
 
+const TRACK_SUBSCRIPTION = gql`
+  subscription OnTrackViewsUpdated($trackId: ID!) {
+    trackViewsUpdated(id: $trackId) {
+      id
+      numberOfViews
+    }
+  }
+`;
+
 /**
  * Track Page fetches a track's data from the gql query GET_TRACK
  * and provides it to the TrackDetail component to display
  */
 const Track = ({ trackId }) => {
-  const { loading, error, data } = useQuery(GET_TRACK, {
+  const { loading, error, data, subscribeToMore } = useQuery(GET_TRACK, {
     variables: { trackId },
   });
+
+  useEffect(() => {
+    console.log(`subscribing to ${trackId}...`);
+    const unsubscribe = subscribeToMore({
+      document: TRACK_SUBSCRIPTION,
+      variables: { trackId },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        return {
+          ...prev,
+          track: {
+            ...prev.track,
+            numberOfViews:
+              subscriptionData.data.trackViewsUpdated.numberOfViews,
+          },
+        };
+      },
+    });
+    console.log(`subscribed to ${trackId}.`);
+
+    return () => {
+      console.log(`unsubscribing for ${trackId}...`);
+      unsubscribe();
+      console.log(`unsubscribed for ${trackId}.`);
+    };
+  }, [subscribeToMore, trackId]);
 
   return (
     <Layout>
